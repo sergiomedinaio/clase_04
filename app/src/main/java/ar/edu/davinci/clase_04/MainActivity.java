f3f3b3483143186d43da211af1598054c20688e8
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-09-13 22:39:55 -0300

    feat: add textview on runtime

--- app/src/main/java/ar/edu/davinci/clase_04/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/clase_04/MainActivity.java
@@ -3,6 +3,8 @@ package ar.edu.davinci.clase_04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout content = (LinearLayout) findViewById(R.id.content);
+
+        TextView primerTexto = new TextView(this);
+        primerTexto.setText("Primer elemento de la lista");
+        content.addView(primerTexto);
+
+        TextView segundoTexto = new TextView(this);
+        segundoTexto.setText("Segundo elemento de la lista");
+        content.addView(segundoTexto);
+
     }
 }
